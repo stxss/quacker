dee660c2992fc28db14ec6df21b2774633e539bc
stxss <93729036+stxss@users.noreply.github.com>
2023-05-21 21:41:04 +0100

    Add a route for editing the current user's profile, disabling the resources#edit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "tweets#index"
-  resources :users do
+  resources :users, except: %i[ edit ] do
     member do
       get "following", "followers"
     end
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   resources :follows, only: %i[create destroy]
   resources :notifications, only: %i[index]
 
+  get "/settings/profile", to: "users#edit", as: "settings"
   get "/:username", to: "users#show", as: "username"
 end
