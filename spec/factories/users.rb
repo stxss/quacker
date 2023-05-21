d915764dcf9fcbb4d73ef1f1aed7860ee064289f
stxss <93729036+stxss@users.noreply.github.com>
2023-05-21 19:09:14 +0100

    Add initial factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.safe_email }
+    username { Faker::Internet }
+    password { Faker::Internet.password(min_length: 6) }
   end
 end
