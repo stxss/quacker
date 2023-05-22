fa18194a0d3dec11e2ad342b04c1367ed0456e3c
stxss <93729036+stxss@users.noreply.github.com>
2023-05-22 18:37:08 +0100

    Update User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.safe_email }
-    username { Faker::Internet }
+    email { Faker::Internet.email }
+    username { Faker::Internet.username }
     password { Faker::Internet.password(min_length: 6) }
   end
 end
