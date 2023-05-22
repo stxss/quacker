c76c0e8af6a0fb76f51c0f5ce9ad591780ef0c01
stxss <93729036+stxss@users.noreply.github.com>
2023-05-22 18:38:00 +0100

    Create a model test for follow checking

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#follow" do
+    let!(:first) { create(:user) }
+    let!(:second) { create(:user) }
+
+    before do
+      create(:follow, follower_id: first.id, followed_id: second.id)
+    end
+
+    it "creates a following" do
+      expect(first).to be_following(second)
+    end
+  end
 end
