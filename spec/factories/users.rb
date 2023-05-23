fca420182a57e7b06844138b9c706f2f43054d1e
stxss <93729036+stxss@users.noreply.github.com>
2023-05-23 12:07:25 +0100

    Change the Factory to not allow dots as separators

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    username { Faker::Internet.username }
+    username { Faker::Internet.username(specifier: 4..15, separators: ["_"]) }
     password { Faker::Internet.password(min_length: 6) }
   end
 end
