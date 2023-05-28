1f0a7ac5d7f7bd5fff2736b3c2457d68c9aab4b1
stxss <93729036+stxss@users.noreply.github.com>
2023-05-28 19:49:46 +0100

    Update boolean values to not accept null

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_27_140230) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_28_125834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "photo_tagging", default: 0
+    t.integer "allow_media_tagging", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "sensitive_tweets", default: false
-    t.boolean "display_sensitive_tweets", default: false
-    t.boolean "remove_blocked_and_muted_accounts", default: true
-    t.boolean "muted_notif_you_dont_follow", default: false
-    t.boolean "muted_notif_dont_follow_you", default: false
-    t.boolean "muted_notif_new_account", default: false
-    t.boolean "muted_notif_default_profile_pic", default: false
-    t.boolean "muted_notif_no_confirm_email", default: false
-    t.boolean "allow_message_request_from_everyone", default: false
-    t.boolean "show_read_receipts", default: false
+    t.boolean "sensitive_tweets", default: false, null: false
+    t.boolean "display_sensitive_tweets", default: false, null: false
+    t.boolean "remove_blocked_and_muted_accounts", default: true, null: false
+    t.boolean "muted_notif_you_dont_follow", default: false, null: false
+    t.boolean "muted_notif_dont_follow_you", default: false, null: false
+    t.boolean "muted_notif_new_account", default: false, null: false
+    t.boolean "muted_notif_default_profile_pic", default: false, null: false
+    t.boolean "muted_notif_no_confirm_email", default: false, null: false
+    t.boolean "allow_message_request_from_everyone", default: false, null: false
+    t.boolean "show_read_receipts", default: false, null: false
     t.boolean "private_visibility", default: false
     t.index ["user_id"], name: "index_accounts_on_user_id", unique: true
   end
