072a7f436bf2c304de076d2d6e4bddb45253e096
stxss <93729036+stxss@users.noreply.github.com>
2023-06-02 23:29:15 +0100

    Add character counter to tweet text

--- app/javascript/controllers/tweet_controller.js
+++ app/javascript/controllers/tweet_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to the media tagging setting
 export default class extends Controller {
-    static targets = [ "area" ];
+    static targets = [ "area", "counter" ];
 
     connect() {
         this.verifyLength()
@@ -10,10 +10,27 @@ export default class extends Controller {
 
     verifyLength() {
         let area = this.areaTarget
-        let length = area.querySelector("textarea").value.length
+        let content = area.querySelector("textarea").value
+        let length = content.length
         let button = document.querySelector("#submit-tweet")
 
         button.disabled = length < 1 || length > 280
 
+        this.showCount(length)
+    }
+
+    showCount(length) {
+        console.log(length)
+        if (length < 260) {
+            this.counterTarget.textContent = ""
+        } else {
+            this.counterTarget.textContent = 280 - length
+        }
+
+        if (length >= 280) {
+            this.counterTarget.style = "color: red"
+        } else {
+            this.counterTarget.style = "color: black"
+        }
     }
 }
