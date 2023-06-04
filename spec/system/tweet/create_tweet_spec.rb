1c67ee68d8b6cb21bd622a06a65a5807e9547455
stxss <93729036+stxss@users.noreply.github.com>
2023-06-04 14:33:42 +0100

    Create some tweet tests

--- spec/system/tweet/create_tweet_spec.rb
+++ spec/system/tweet/create_tweet_spec.rb
@@ -3,32 +3,43 @@ require 'rails_helper'
 RSpec.describe "Update user account settings", type: :system do
   before do
     driven_by :selenium_chrome_headless
-    # driven_by :selenium_chrome
   end
+
   let!(:user) { create(:user) }
 
-  it "creates a normal tweet" do
+  before do
     login_as user
     visit root_path
+  end
+
+  it "creates a normal tweet" do
     fill_in "tweet_body", with: "First tweet!"
     click_on "Tweet"
     expect(page).to have_css("#tweet", text: "First tweet!")
   end
 
   it "creates a tweet with a single character" do
-    login_as user
-    visit root_path
     fill_in "tweet_body", with: "1"
     click_on "Tweet"
     expect(page).to have_css("#tweet", text: "1")
   end
 
-  it "doesn't create a tweet with 0 non-space characters" do
-    login_as user
-    visit root_path
+  it "doesn't create a tweet with only whitespace characters" do
     fill_in "tweet_body", with: "     "
-    click_on "Tweet"
-    expect(page).not_to have_css("#tweet", text: "     ")
+    expect(page).not_to have_css("#real-submit-tweet")
+    expect(page).to have_css("#fake-submit-tweet")
   end
 
+  it "does create a tweet with exactly 280 characters" do
+    exact_limit_tweet = Faker::Hipster.paragraph_by_chars(characters: 280)
+    fill_in "tweet_body", with: exact_limit_tweet
+    click_on "Tweet"
+    expect(page).to have_css("#tweet", text: exact_limit_tweet)
+  end
+
+  it "doesn't create a tweet with more than 280 of characters" do
+    fill_in "tweet_body", with: Faker::Hipster.paragraph_by_chars(characters: 281)
+    expect(page).not_to have_css("#real-submit-tweet")
+    expect(page).to have_css("#fake-submit-tweet")
+  end
 end
