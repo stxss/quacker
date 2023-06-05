c22543636530b82e001d2df7db0d82bdbaff77d3
stxss <93729036+stxss@users.noreply.github.com>
2023-06-05 14:20:30 +0100

    Remove a method call

--- app/javascript/controllers/tweet_controller.js
+++ app/javascript/controllers/tweet_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
     static targets = [ "area", "counter", "circle", "submit" ];
 
     connect() {
-        this.verifyLength()
     }
 
     verifyLength() {
