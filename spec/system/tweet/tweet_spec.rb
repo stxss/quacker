f1b01d2152ac212c4c52cb5277c94e3dafb93bcd
stxss <93729036+stxss@users.noreply.github.com>
2023-06-15 19:39:41 +0100

    Modify and add specs

--- spec/system/tweet/tweet_spec.rb
+++ spec/system/tweet/tweet_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "Tweet creation", type: :system do
   end
 
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:private_user) { create(:user) }
 
   before do
     login_as user
@@ -18,6 +20,12 @@ RSpec.describe "Tweet creation", type: :system do
     fill_in "tweet_body", with: "First tweet!"
     click_on "Tweet"
     expect(page).to have_css("#tweet-body", text: "First tweet!")
+  end
+
+  it "can visit tweet" do
+    tweet = create(:tweet, user_id: user.id, body: "Test tweet")
+    visit single_tweet_path(username: user.username, id: tweet.id)
+    expect(page).to have_content("Test tweet")
   end
 
   it "creates a tweet with a single character" do
@@ -47,7 +55,7 @@ RSpec.describe "Tweet creation", type: :system do
 
   it "shows a tweet created minutes ago" do
     content = Faker::Hipster.paragraph_by_chars(characters: 100)
-    test_tweet =  user.created_tweets.create(body: content)
+    test_tweet = user.created_tweets.create(body: content)
     test_tweet.update(created_at: 2.minutes.ago)
     visit root_path
     expect(page).to have_css("#tweet-body", text: content)
@@ -56,7 +64,7 @@ RSpec.describe "Tweet creation", type: :system do
 
   it "shows a tweet created hours ago" do
     content = Faker::Hipster.paragraph_by_chars(characters: 100)
-    test_tweet =  user.created_tweets.create(body: content)
+    test_tweet = user.created_tweets.create(body: content)
     test_tweet.update(created_at: 2.hours.ago)
     visit root_path
     expect(page).to have_css("#tweet-body", text: content)
@@ -65,7 +73,7 @@ RSpec.describe "Tweet creation", type: :system do
 
   it "shows a tweet created this year" do
     content = Faker::Hipster.paragraph_by_chars(characters: 100)
-    test_tweet =  user.created_tweets.create(body: content)
+    test_tweet = user.created_tweets.create(body: content)
     test_tweet.update(created_at: Date.new(Time.zone.now.year, 4, 7))
     visit root_path
     expect(page).to have_css("#tweet-body", text: content)
@@ -74,7 +82,7 @@ RSpec.describe "Tweet creation", type: :system do
 
   it "shows a tweet created last year" do
     content = Faker::Hipster.paragraph_by_chars(characters: 100)
-    test_tweet =  user.created_tweets.create(body: content)
+    test_tweet = user.created_tweets.create(body: content)
     test_tweet.update(created_at: Date.new(Time.zone.now.year - 1, 11, 7))
     visit root_path
     expect(page).to have_css("#tweet-body", text: content)
@@ -88,7 +96,129 @@ RSpec.describe "Tweet creation", type: :system do
     expect(page).to have_css("#tweet-body", text: "First tweet!")
   end
 
-  xit "deletes tweet" do
+  it "when author, shows delete button" do
+    fill_in "tweet_body", with: "First tweet!"
+    click_on "Tweet"
+    visit username_path(user.username)
+    expect(page).to have_button("Delete")
+  end
 
+  it "when author can delete tweet" do
+    fill_in "tweet_body", with: "First tweet!"
+    click_on "Tweet"
+    visit username_path(user.username)
+    click_on "Delete"
+    within "#turbo-confirm" do
+      click_on "Delete"
+    end
+    expect(page).not_to have_css("#tweet-body", text: "First tweet!")
+  end
+
+  it "when not author doesn't show delete button" do
+    fill_in "tweet_body", with: "First tweet!"
+    click_on "Tweet"
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+    expect(page).not_to have_button("Delete")
+  end
+
+  it "user can retweet own tweets" do
+    visit root_path
+    fill_in "tweet_body", with: "Public tweet!"
+    click_on "Tweet"
+    click_on "rt"
+    visit root_path
+    within ".retweet-info" do
+      expect(page).to have_css("#display-name", text: user.display_name).twice
+      expect(page).to have_css("#tweet-body", text: "Public tweet!")
+    end
+  end
+
+  it "another user can retweet" do
+    visit root_path
+    fill_in "tweet_body", with: "Public tweet!"
+    click_on "Tweet"
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+    click_on "rt"
+    visit root_path
+    within ".retweet-info" do
+      expect(page).to have_css("#display-name", text: other_user.display_name).once
+      expect(page).to have_css("#display-name", text: user.display_name).once
+      expect(page).to have_css("#tweet-body", text: "Public tweet!")
+    end
+  end
+
+  it "user can't retweet deleted tweet" do
+    visit root_path
+    fill_in "tweet_body", with: "Public tweet!"
+    click_on "Tweet"
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+    user.created_tweets.last.destroy
+    click_on "rt"
+    expect(page).to have_content("Couldn't retweet")
+  end
+
+  it "user can unretweet" do
+    visit root_path
+    fill_in "tweet_body", with: "Public tweet!"
+    click_on "Tweet"
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+    within ".retweets" do
+      find("#retweet").click
+    end
+    visit root_path
+    find("#unretweet").click
+    expect(page).not_to have_css("#display-name", text: other_user.display_name).once
+    expect(page).not_to have_css("#display-name", text: user.display_name).once
+    expect(page).not_to have_css("#tweet-body", text: "Public tweet!")
+  end
+
+  xit "private account retweets" do
+  end
+
+  it "tweets have like button" do
+    visit root_path
+    fill_in "tweet_body", with: "First tweet!"
+    click_on "Tweet"
+    expect(page).to have_css("#like")
+  end
+
+  it "authors can like their own tweets" do
+    visit root_path
+    fill_in "tweet_body", with: "First tweet!"
+    click_on "Tweet"
+    visit username_path(user.username)
+    find("#like").click
+    expect(page).to have_css("#unlike")
+  end
+
+  it "users can like others' tweets" do
+    visit root_path
+    fill_in "tweet_body", with: "First tweet!"
+    click_on "Tweet"
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+    find("#like").click
+    expect(page).to have_css("#unlike")
+  end
+
+  it "users cannot like deleted tweets" do
+    visit root_path
+    fill_in "tweet_body", with: "Public tweet!"
+    click_on "Tweet"
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+    user.created_tweets.last.destroy
+    find("#like").click
+    expect(page).to have_content("Couldn't like")
   end
 end
---
+++ spec/system/user/static_pages_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe "Check user static pages", type: :system do
+  before do
+    driven_by :selenium_chrome_headless
+    # driven_by :selenium_chrome
+  end
+
+  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+
+  before do
+    login_as user
+    visit root_path
+  end
+
+  it "displays user liked tweets" do
+    tweets = create_list(:tweet, 5, user_id: user.id, body: "Test tweet ")
+
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+
+    # Find the first three tweets and like them
+    tweets[0..2].each do |tweet|
+      within("#tweet_#{tweet.id}") do
+        find("#like").click
+      end
+    end
+
+    # Assert that the first three tweets are liked
+    tweets[0..2].each do |tweet|
+      within("#tweet_#{tweet.id}") do
+        expect(page).to have_css("#unlike")
+      end
+    end
+
+    # Assert that the remaining tweets are not liked
+    tweets[3..4].each do |tweet|
+      within("#tweet_#{tweet.id}") do
+        expect(page).to have_css("#like")
+      end
+    end
+
+    visit username_path(other_user.username)
+    click_on "Likes"
+    expect(page).to have_css("#tweet-body").thrice
+  end
+
+  xit "displays user tweets and retweets" do
+    tweets = create_list(:tweet, 10, user_id: user.id, body: "Test tweet ")
+
+    visit root_path
+    login_as other_user
+    visit username_path(user.username)
+
+    # Find the first five tweets and retweet them
+    tweets[0..5].each do |tweet|
+      within("#tweet_#{tweet.id}") do
+        find("#retweet").click
+      end
+    end
+
+    # Assert that the first five tweets are retweeted
+    tweets[0..5].each do |tweet|
+      within("#tweet_#{tweet.id}") do
+        expect(page).to have_css("#unretweet")
+      end
+    end
+
+    # Assert that the remaining tweets are not retweeted
+    tweets[6..9].each do |tweet|
+      within("#tweet_#{tweet.id}") do
+        expect(page).to have_css("#retweet")
+      end
+    end
+
+    visit username_path(other_user.username)
+    expect(page).to have_css("#tweet-body").thrice
+  end
+end
