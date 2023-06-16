9c7f3deb5560509dc30828a1a322530840c3c85f
stxss <93729036+stxss@users.noreply.github.com>
2023-06-16 21:54:20 +0100

    Add unique constraint to likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_214340) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_181638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_214340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes_count", default: 0
+    t.index ["tweet_id", "user_id"], name: "index_likes_on_tweet_id_and_user_id", unique: true
   end
 
   create_table "notifications", force: :cascade do |t|
