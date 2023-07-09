4c133bd8ac2862b8987cedf2dfa6de7e4cdbd7d1
stxss <93729036+stxss@users.noreply.github.com>
2023-07-09 23:09:11 +0100

    Add counter cache to quotes and retweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,8 +10,8 @@ class Tweet < ApplicationRecord
   has_many :comments, class_name: "Tweet", foreign_key: :parent_tweet_id
   belongs_to :parent, class_name: "Tweet", optional: true, counter_cache: true
 
-  has_many :retweets, class_name: "Tweet", foreign_key: :retweet_id, dependent: :destroy
-  has_many :quote_tweets, class_name: "Tweet", foreign_key: :quoted_retweet_id, dependent: :destroy
+  has_many :retweets, class_name: "Tweet", foreign_key: :retweet_id, dependent: :destroy, counter_cache: :retweets_count
+  has_many :quote_tweets, class_name: "Tweet", foreign_key: :quoted_retweet_id, dependent: :destroy, counter_cache: :quote_tweets_count
 
   # default_scope { order(created_at: :desc) }
   default_scope { order(updated_at: :desc) }
