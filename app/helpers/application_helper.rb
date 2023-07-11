939252f67cd44986e7aa30e9d96b411903ccacce
stxss <93729036+stxss@users.noreply.github.com>
2023-07-11 22:38:11 +0100

    Create file for the 'more info' dots

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,6 +35,18 @@ module ApplicationHelper
     end
   end
 
+  def more_info_svg
+    content_tag(:svg, id: "icon-more-info") do
+      content_tag(:path, "", d: "M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z")
+    end
+  end
+
+  # def svg
+  #   content_tag(:svg, id: "icon-") do
+  #     content_tag(:path, "", d: "")
+  #   end
+  # end
+
   def formatted_time_ago(created_at)
     seconds_ago = (Time.current - created_at).to_i
 
