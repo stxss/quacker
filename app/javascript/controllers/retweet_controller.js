680b23a53faa3e5f1dc227af851cd0e0058dc4ee
stxss <93729036+stxss@users.noreply.github.com>
2023-07-19 23:43:38 +0100

    Add trap focus

--- app/javascript/controllers/retweet_controller.js
+++ app/javascript/controllers/retweet_controller.js
@@ -21,6 +21,51 @@ export default class extends Controller {
     }
 
     dropMenu() {
+        this.trapFocus(this.element)
         this.menuTarget.classList.toggle("show");
+    }
+
+
+    trapFocus(element) {
+        // https://hidde.blog/using-javascript-to-trap-focus-in-an-element/
+
+        let focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+        let firstFocusableEl = focusableEls[0];
+        let lastFocusableEl = focusableEls[focusableEls.length - 1];
+        let KEYCODE_TAB = 9;
+
+        // document.addEventListener("click", (event) => {
+        //     let withinBoundaries = event.composedPath().includes(this.element);
+
+        //     if (!withinBoundaries) {
+        //         console.log(focusableEls)
+        //         let isEqual = Array.from(focusableEls).some(node => node === event.target)
+        //         console.log(isEqual)
+        //         if (!isEqual) {
+        //             event.preventDefault()
+        //         }
+        //         this.menuTarget.classList.remove("show");
+        //     }
+        // });
+
+        element.addEventListener('keydown', function(e) {
+          let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+          if (!isTabPressed) {
+            return;
+          }
+
+          if ( e.shiftKey ) /* shift + tab */ {
+            if (document.activeElement === firstFocusableEl) {
+              lastFocusableEl.focus();
+                e.preventDefault();
+              }
+            } else /* tab */ {
+            if (document.activeElement === lastFocusableEl) {
+              firstFocusableEl.focus();
+                e.preventDefault();
+              }
+            }
+        });
     }
 }
