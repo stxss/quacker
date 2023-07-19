a617f43474de2132c590cd73a06f1c4772eda2ad
stxss <93729036+stxss@users.noreply.github.com>
2023-07-19 21:31:44 +0100

    Update dialog windows closing behavior
    The user can now close the window when clicking outside the dialog or
    pressing the escape key

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,16 +6,24 @@ import "controllers"
 Turbo.setConfirmMethod(() => {
     let dialog = document.getElementById("turbo-confirm")
     dialog.showModal()
+    document.documentElement.style.overflow = "hidden"
 
     // Specifically for the checkbox when setting the protected tweets setting on
     let diagCheckBox = document.getElementById("account_private_visibility")
     if (diagCheckBox) {
         diagCheckBox.checked = false
     }
+    
+    dialog.addEventListener("click", (event) => {
+        if (event.target === dialog) {
+            dialog.close()
+        }
+    });
 
     return new Promise((resolve, reject) => {
         dialog.addEventListener("close", () => {
             resolve(dialog.returnValue == "confirm")
+            document.documentElement.removeAttribute("style");
         }, { once: true })
     })
 })
