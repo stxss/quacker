71610e46c7ca920f1055f4f658bb770ad4476728
stxss <93729036+stxss@users.noreply.github.com>
2023-07-30 23:30:30 +0100

    Update backdrop menu when opening RT menu

--- app/javascript/controllers/retweet_controller.js
+++ app/javascript/controllers/retweet_controller.js
@@ -21,50 +21,39 @@ export default class extends Controller {
     }
 
     dropMenu() {
-        this.trapFocus(this.element)
         this.menuTarget.classList.toggle("show");
+        this.trapFocus(this.element);
     }
-
 
     trapFocus(element) {
         // https://hidde.blog/using-javascript-to-trap-focus-in-an-element/
 
-        let focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+        let focusableEls = element.querySelectorAll(
+            'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
+        );
         let firstFocusableEl = focusableEls[0];
         let lastFocusableEl = focusableEls[focusableEls.length - 1];
         let KEYCODE_TAB = 9;
 
-        // document.addEventListener("click", (event) => {
-        //     let withinBoundaries = event.composedPath().includes(this.element);
+        element.addEventListener("keydown", function (e) {
+            let isTabPressed = e.key === "Tab" || e.keyCode === KEYCODE_TAB;
 
-        //     if (!withinBoundaries) {
-        //         console.log(focusableEls)
-        //         let isEqual = Array.from(focusableEls).some(node => node === event.target)
-        //         console.log(isEqual)
-        //         if (!isEqual) {
-        //             event.preventDefault()
-        //         }
-        //         this.menuTarget.classList.remove("show");
-        //     }
-        // });
+            if (!isTabPressed) {
+                return;
+            }
 
-        element.addEventListener('keydown', function(e) {
-          let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-
-          if (!isTabPressed) {
-            return;
-          }
-
-          if ( e.shiftKey ) /* shift + tab */ {
-            if (document.activeElement === firstFocusableEl) {
-              lastFocusableEl.focus();
-                e.preventDefault();
-              }
-            } else /* tab */ {
-            if (document.activeElement === lastFocusableEl) {
-              firstFocusableEl.focus();
-                e.preventDefault();
-              }
+            if (e.shiftKey) {
+                /* shift + tab */ if (
+                    document.activeElement === firstFocusableEl
+                ) {
+                    lastFocusableEl.focus();
+                    e.preventDefault();
+                }
+            } /* tab */ else {
+                if (document.activeElement === lastFocusableEl) {
+                    firstFocusableEl.focus();
+                    e.preventDefault();
+                }
             }
         });
     }
