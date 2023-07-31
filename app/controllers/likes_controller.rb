d001d44e56df73b401992f292ccef9209639c3b6
stxss <93729036+stxss@users.noreply.github.com>
2023-07-31 20:26:45 +0100

    Update likes and its turbo streams to update multiple items at the same time

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,14 +5,7 @@ class LikesController < ApplicationController
 
     respond_to do |format|
       if @like.save
-        format.turbo_stream {
-          render turbo_stream: [
-            turbo_stream.replace("tweet_#{@tweet.id}", partial: "tweets/single_tweet", locals: {t: @tweet}),
-            turbo_stream.update("like_count_#{@tweet.id}", partial: "tweets/like_count", locals: {t: @tweet}),
-            turbo_stream.update("like_#{@tweet.id}", partial: "tweets/unlike_button", locals: {tweet: @tweet})
-          ]
-        }
-
+        format.turbo_stream
         format.html { redirect_to request.referrer }
         current_user.notify(@tweet.author.id, :like, tweet_id: @tweet.id)
       # else
@@ -40,13 +33,7 @@ class LikesController < ApplicationController
     @og = @like.tweet
 
     respond_to do |format|
-      format.turbo_stream {
-        render turbo_stream: [
-          turbo_stream.replace("tweet_#{@og.id}", partial: "tweets/single_tweet", locals: {t: @og}),
-          turbo_stream.update("like_count_#{@og.id}", partial: "tweets/like_count", locals: {t: @og}),
-          turbo_stream.update("like_#{@og.id}", partial: "tweets/like_button", locals: {tweet: @og})
-        ]
-      }
+      format.turbo_stream
       format.html { redirect_to request.referrer }
       @tweet.author.notifications_received.where(notifier_id: current_user.id, notification_type: :like, tweet_id: @tweet.id).destroy_all
     end
