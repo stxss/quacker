b7b7e401592fb7a57cd9e07ed79f6964702cad32
stxss <93729036+stxss@users.noreply.github.com>
2023-08-05 01:21:09 +0100

    Set up broadcasts for retweets and update feature
    Include broadcasting for the retweets, in a similar way to the likes,
    where the counters are updated globally, also tackling the problem where
    a user tries to "toy" with the retweets on separate tabs.
    
    Also include a check and prevention of the issue for the case if the
    author sets their account to private and a user that sees the authors
    tweet still has the not updated privated version of the button.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  class UserGonePrivate < StandardError; end
+
   before_action :first_visit?, :set_cache_headers, only: [:index]
   before_action :check_refresh, :session_refresh, only: [:new]
   after_action :refresh_comments, only: [:index]
@@ -44,22 +46,56 @@ class TweetsController < ApplicationController
   end
 
   def retweet
+    # When user clicks retweet, it looks up the tweet, creates a retweet and then broadcasts the updated count to all the users. Finally, to update the buttons correctly for the user, a format.turbo_stream request is made with the value of the tweet as the original has been updated with a retweet count. By fetching the original tweet after the update, the most recent values are assured and errors minimized
+
     @retweet = current_user.created_tweets.build(retweet_params)
     @tweet = @retweet.og_tweet || Tweet.find(retweet_params[:retweet_id])
 
+    @tweet.broadcast_render_later_to "retweets",
+      partial: "tweets/update_retweets_count",
+      locals: {t: Tweet.find(@tweet.id)}
+
+    if @tweet.author.account.private_visibility && current_user != @tweet.author
+      raise UserGonePrivate
+    end
+
     respond_to do |format|
       if @retweet.save
-        format.turbo_stream
+        format.turbo_stream {
+          render turbo_stream: [
+            turbo_stream.replace_all("#retweet_#{@tweet.id}", partial: "tweets/retweets", locals: {t: @tweet, user: current_user})
+          ]
+        }
         format.html { redirect_to request.referrer }
         current_user.notify(@tweet.author.id, :retweet, tweet_id: @tweet.id)
       end
     end
+  rescue ActiveRecord::RecordNotUnique
+    # If a user already has a retweet, it would invoke a ActiveRecord::RecordNotUnique error, so in that case, no data manipulation is to happen and a turbo request for a simple visual update is made
+
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: [
+          turbo_stream.replace_all("#retweet_#{@tweet.id}", partial: "tweets/retweets", locals: {t: @tweet, user: current_user})
+        ]
+      }
+    end
   rescue ActiveRecord::RecordNotFound
+    # ! LOOK THIS UP
     respond_to do |format|
       format.turbo_stream {
         render turbo_stream: [
           turbo_stream.remove("tweet_#{retweet_params[:retweet_id]}"),
           flash.now[:alert] = "Couldn't retweet"
+        ]
+      }
+    end
+  rescue UserGonePrivate
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: [
+          turbo_stream.replace_all("#retweet_#{@tweet.id} .dropdown", partial: "tweets/fake_retweet_menu", locals: {t: @tweet}),
+          flash.now[:alert] = "Couldn't retweet a privated tweet"
         ]
       }
     end
@@ -82,20 +118,39 @@ class TweetsController < ApplicationController
   end
 
   def destroy_retweet
-    @tweet = current_user.created_tweets.find_by(retweet_id: retweet_params[:retweet_id])
-    @tweet.destroy
-    @og = Tweet.find(@tweet.retweet_id)
+    # When user clicks undo retweet, it looks up the tweet, destroys the retweet and then broadcasts the updated count to all the users. Finally, to update the buttons correctly for the user, a format.turbo_stream request is made with the value of @og_updated By using @og_updated (meaning, the tweet after the unretweet action), it is ensured that the most updated version of the count is shown
 
-    @button_update = if @og.author.account.private_visibility && current_user != @og.author
-      turbo_stream.update_all(".retweets #retweet_#{@og.id}", partial: "tweets/fake_retweet_menu", locals: {t: @og})
-    else
-      turbo_stream.update_all(".retweets #retweet_#{@og.id}", partial: "tweets/drop_menu", locals: {t: @og})
-    end
+    @og = Tweet.find(retweet_params[:retweet_id])
+
+    @retweet = current_user.created_tweets.find_by(retweet_id: retweet_params[:retweet_id])
+    @retweet.destroy
+
+    @og_updated = @retweet.og_tweet || Tweet.find(@retweet.retweet_id)
+
+    @og_updated.broadcast_render_later_to "retweets",
+      partial: "tweets/update_retweets_count",
+      locals: {t: Tweet.find(@og_updated.id)}
 
     respond_to do |format|
-      format.turbo_stream
       format.html { redirect_to request.referrer }
       @tweet.author.notifications_received.where(notifier_id: current_user.id, notification_type: :retweet, tweet_id: @tweet.id).destroy_all
+    end
+  rescue NoMethodError
+    # If a user did already unretweet (same session on 2 different tabs for example), it would invoke a NoMethodError, as @retweet would return nil and nil can't have a #destroy method executed on it, so in that case, no data manipulation is to happen and a turbo request for a simple visual update is made with the original version - @og. @og_updated couldn't be used as it is only defined after the unretweet is made
+
+    respond_to do |format|
+      @to_remove = if @retweet
+        turbo_stream.remove("tweet_#{@retweet.id}")
+      else
+        turbo_stream.remove("tweet_#{retweet_params[:self_rt]}")
+      end
+
+      format.turbo_stream {
+        render turbo_stream: [
+          @to_remove,
+          turbo_stream.replace_all("#retweet_#{@og.id}", partial: "tweets/retweets", locals: {t: @og, user: current_user})
+        ]
+      }
     end
   end
 
@@ -119,7 +174,7 @@ class TweetsController < ApplicationController
   end
 
   def retweet_params
-    params.require(:retweet).permit(:retweet_id)
+    params.require(:retweet).permit(:retweet_id, :self_rt)
   end
 
   def first_visit?
