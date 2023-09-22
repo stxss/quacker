d4d8f70dcc4fe006e2ee519114eead177600baa1
stxss <93729036+stxss@users.noreply.github.com>
2023-09-22 21:50:42 +0100

    Update conversation related models

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,14 +5,25 @@ class Conversation < ApplicationRecord
   has_many :members, class_name: "User", foreign_key: :user_id, through: :conversation_members
   has_many :messages, dependent: :destroy
 
-  validate :group_name_presence_if_multiple_members
+  before_create :validate_group_name_presence_if_multiple_members
+  before_create :validate_minimum_members
+
+  # validates :conversation_members, uniqueness: {scope: :conversation_id}
+  scope :find_by_members, ->(member_ids) { joins(:members).where(users: {id: member_ids}).group("conversations.id").having("COUNT(conversations.id) = ?", member_ids.size).first }
 
   private
 
-  def group_name_presence_if_multiple_members
-    p members
+  def validate_group_name_presence_if_multiple_members
     if members.size > 2 && name.blank?
       errors.add(:name, "must be present when there are 2 or more members")
     end
   end
+
+  def validate_minimum_members
+    if members.size < 2
+      errors.add(:members, "must be at least 2")
+    end
+  end
 end
+
+# Handle error messages with flash messages
--- app/models/conversation_member.rb
+++ app/models/conversation_member.rb
@@ -1,4 +1,6 @@
 class ConversationMember < ApplicationRecord
+  self.primary_key = :member_id
+
   belongs_to :member, class_name: "User"
   belongs_to :conversation
 end
