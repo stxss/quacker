6e97ff6d28c5625ae225eac563f3392a8cf450fb
stxss <93729036+stxss@users.noreply.github.com>
2023-10-14 18:05:09 +0100

    Update messages and conversations
    In conversations, just updated for it to scroll to the bottom when the
    user opens some conversation. With messages, added the verification for
    an empty message not being able to be sent

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -1,14 +1,61 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="message"
 export default class extends Controller {
-  static targets = ["area"]
-  connect() {
+    static targets = ["area", "list", "submit"];
+
+    initialize() {
+        this.update();
+    }
+
+    connect() {
+        this.popStateHandler = (e) => this.clear(e);
+        window.addEventListener("popstate", this.popStateHandler);
+        this.update();
+    }
+
+    update() {
+        this.verifyLength(this.length);
+    }
+
+    verifyLength(length) {
+        if (this.notMessage()) {
+            if (!this.submitTarget.querySelector(".fake-submit-message")) {
+                this.submitTarget.innerHTML = `<div class="fake-submit-message">Send</div>`;
+            }
+        } else {
+            if (!this.submitTarget.querySelector("input")) {
+                this.submitTarget.innerHTML = `<input type="submit" name="commit" value="Send" class="w-fit p-3" data-disable-with="Create Message" data-action="click->message#submit"></input>
+                `;
+            }
+        }
+    }
+
+    submit(e) {
+        if (this.notMessage()) {
+            e.preventDefault();
+        }
+    }
+
+    notMessage() {
+        return (
+            this.content.match(/^\s*$/) || this.length < 1
+        );
+    }
+
+    clear() {
+      if (this.areaTarget.value != "") {
+          this.areaTarget.value = "";
+      }
+      window.removeEventListener("popstate", this.popStateHandler);
+      this.update();
   }
 
-  clear() {
-    if (this.areaTarget.value != "") {
-      this.areaTarget.value = "";
+    get content() {
+        return this.areaTarget.value;
     }
-  }
+
+    get length() {
+        return this.content.length;
+    }
 }
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
+  validates :body, presence: true
+  validates :body, length: {minimum: 1, message: "Can't send an empty message"}
+
   belongs_to :sender, class_name: "User"
   belongs_to :conversation, counter_cache: true
   scope :unread, -> { where(read: false) }
