39690bb76cf07053d21c80f7f2b391aa7c1c284d
stxss <93729036+stxss@users.noreply.github.com>
2023-10-28 18:05:21 +0100

    Introduce internal link embeds in direct messages
    When a user shares a link in the dm's, if that link is a link from a
    post on the platform, similar to when a user on twitter shares a tweet
    with someone else via Twitter Direct Messages, it is embedded in a
    specific way. So I introduced the same feature

--- app/models/concerns/reusable.rb
+++ app/models/concerns/reusable.rb
@@ -1,6 +1,39 @@
 module Reusable
+  # change host to root_url later, as in development it's localhost:3000 and its tricky to use
+  # INTERNAL_HOST = ActionMailer::Base.default_url_options[:host]
+  INTERNAL_HOST = "example.com"
+  INTERNAL_SCHEME = "https"
+  URL_REGEX = /(https?:\/\/)?\w*\.\w+(\.\w+)*(\/\w+)*(\.\w*)?/
+  USERNAME_POST_REGEX = /\/([^\/]+)\/status\/(\d+)/
+
+  def parse_url(url)
+    uri = URI.parse(url)
+    uri.scheme ||= INTERNAL_SCHEME
+    uri.host ||= INTERNAL_HOST
+    p uri
+  end
+
+  def find_post_by_internal_url(root_url = nil)
+    body.gsub(URL_REGEX) do |url|
+      @url = parse_url(url)
+      return nil if @url.host != INTERNAL_HOST || @url.path.blank?
+
+      @username, @post_id = @url.path.match(USERNAME_POST_REGEX)&.captures
+
+      @user = User.find_by(username: @username) || nil
+      return nil if !@user
+
+      @post = @user.created_tweets.find_by(id: @post_id) || nil
+      return @post unless @post.nil?
+    end
+
+    false
+  rescue URI::BadURIError, URI::InvalidURIError
+    false
+  end
+
   def urlize
-    body.gsub!(/\b(https?:\/\/)?\w+\.\w+(\.\w+)*(\/\w+)*(\.\w*)?\b/) do |url|
+    body.gsub!(URL_REGEX) do |url|
       URI.parse(url).scheme.nil? ? "https://#{url}" : url
     end
 
@@ -13,7 +46,7 @@ module Reusable
   def replace_urls
     extracted = URI.extract(body, ["http", "https"])
     body.split(/\s+/).each_with_index do |word, i|
-      body.gsub!(word, extracted.shift) if word =~ /(https?:\/\/)?\w*\.\w+(\.\w+)*(\/\w+)*(\.\w*)?/
+      body.gsub!(word, extracted.shift) if word.match?(URL_REGEX)
     end
   end
 end
