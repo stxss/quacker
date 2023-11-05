0c76988e7819949e3baa85140031605377b7e601
stxss <93729036+stxss@users.noreply.github.com>
2023-11-05 23:03:27 +0000

    Update index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,29 +7,29 @@ import { application } from "./application"
 import AutogrowController from "./autogrow_controller"
 application.register("autogrow", AutogrowController)
 
+import ConversationController from "./conversation_controller"
+application.register("conversation", ConversationController)
+
 import DropdownController from "./dropdown_controller"
 application.register("dropdown", DropdownController)
 
 import MediaTaggingController from "./media_tagging_controller"
 application.register("media-tagging", MediaTaggingController)
 
-import MiscController from "./misc_controller"
-application.register("misc", MiscController)
-
-import TurboModalController from "./turbo_modal_controller"
-application.register("turbo-modal", TurboModalController)
-
-import TweetController from "./tweet_controller"
-application.register("tweet", TweetController)
-
-import ConversationController from "./conversation_controller"
-application.register("conversation", ConversationController)
-
 import MessageController from "./message_controller"
 application.register("message", MessageController)
+
+import MiscController from "./misc_controller"
+application.register("misc", MiscController)
 
 import ScrollerController from "./scroller_controller"
 application.register("scroller", ScrollerController)
 
 import SearchController from "./search_controller"
 application.register("search", SearchController)
+
+import TurboModalController from "./turbo_modal_controller"
+application.register("turbo-modal", TurboModalController)
+
+import TweetController from "./tweet_controller"
+application.register("tweet", TweetController)
