61a33f6a59aa604112cf426a0ab8fe5bc40a0ba2
stxss <93729036+stxss@users.noreply.github.com>
2023-11-15 00:42:04 +0000

    Replace the circled counter with a numbered one
    As the char limit was bumped up to 10k, a circle counter like twitter
    uses is pretty much useless and ugly. Instead, I opted for a more
    accessible approach, so now a user will see a counter like '1567 / 10000'

--- app/javascript/controllers/tweet_controller.js
+++ app/javascript/controllers/tweet_controller.js
@@ -36,81 +36,23 @@ export default class Tweet extends Controller {
             }
             }
 
-        let circleContainer = this.circleTarget
-
-        if (length === 0 && circleContainer.childNodes.length > 0) {
-            circleContainer.innerHTML = "";
-            } else if (length !== 0 && circleContainer.childNodes.length === 0) {
-            circleContainer.innerHTML = `
-                <svg class="progress-ring" height="100%" width="100%" viewBox="0 0 20 20" style="transform: rotate(-90deg); overflow: visible">
-                <circle class="progress-ring__circle-bg" stroke-width="1.6" fill="transparent" r="10" cx="50%" cy="50%" stroke-linecap="round"></circle>
-                <circle class="progress-ring__circle" stroke-width="2" fill="transparent" r="10" cx="50%" cy="50%" stroke-linecap="round"></circle>
-                <text class="progress-ring__text" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" style="rotate: 90deg; transform-origin: center;"></text>
-                </svg>
-            `;
-        }
         this.showCount(length)
-        this.progressCircle(length);
     }
 
     showCount(length) {
-        this.verifyLength;
-        if (!document.querySelector(".progress-ring")) {
-            return;
-        }
+        // this.verifyLength;
 
-        let text = this.counterTarget.querySelector(".progress-ring__text");
-        if (!text) {return;}
-        let progressRing = document.querySelector(".progress-ring");
+        console.log(this.counterTarget.parentElement)
 
-        if (length < 260) {
-            text.textContent = "";
-            progressRing.style.transition = "width 0.2s, height 0.2s";
-            progressRing.classList.remove("over-limit");
+        if (length > 0) {
+            this.counterTarget.textContent = `${length} / ${this.MAXLENGTH}`
         } else {
-            text.textContent = 7000 - length;
-            text.fontSize = "13px";
-            progressRing.classList.add("over-limit");
+            this.counterTarget.textContent = ""
         }
-
-        if (length >= 7000) {
-            text.style.fill = "red";
+        if (length >= 10000) {
+            this.counterTarget.style.color = "red";
         } else {
-            text.style.fill = "";
-        }
-    }
-
-    progressCircle(length) {
-        if (length === 0) {
-            return;
-        }
-        let circles = this.circleTarget.querySelectorAll("circle");
-        let fgCircle = circles[1];
-        let bgCircle = circles[0];
-        let radius = fgCircle.r.baseVal.value;
-        let circumference = radius * 2 * Math.PI;
-
-        fgCircle.style.strokeDasharray = `${circumference} ${circumference}`;
-        fgCircle.style.strokeDashoffset = `${circumference} `;
-
-        function setProgress(length) {
-            let offset = circumference - (length / 7000) * circumference;
-            if (offset <= 0) {
-                offset = 0;
-                fgCircle.style.stroke = "#F4212E";
-                bgCircle.style.stroke = "#F4212E";
-            } else if (0 < offset && offset <= 4.487989505128276) {
-                fgCircle.style.stroke = "#FFD400";
-                bgCircle.style.stroke = "#2F3336";
-            } else {
-                fgCircle.style.stroke = "#1D9BF0";
-                bgCircle.style.stroke = "#2F3336";
-            }
-            fgCircle.style.strokeDashoffset = offset;
-        }
-
-        setProgress(length);
-    }
+            this.counterTarget.style.color = "white";
         }
     }
 
