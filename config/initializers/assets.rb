ce8db18858100f223715fd52d4f7ea68aafa5c43
stxss <93729036+stxss@users.noreply.github.com>
2023-11-18 21:14:49 +0000

    assets update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,4 @@ Rails.application.config.assets.paths << "./app/assets/stylesheets"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-# Rails.application.config.assets.precompile += %w( application.js application.css )
-Rails.application.config.assets.precompile += ["manifest.js"]
+Rails.application.config.assets.precompile += %w( application.js application.css )
