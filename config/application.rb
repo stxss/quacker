4fb2aec3a1e42ce8ab16301cabacc2fda72c0f3c
stxss <93729036+stxss@users.noreply.github.com>
2023-11-19 16:40:11 +0000

    cable stuff

--- config/application.rb
+++ config/application.rb
@@ -35,10 +35,10 @@ module OdinTwitter
     config.active_job.queue_adapter = :sidekiq
 
     config.action_cable.mount_path = nil
-    config.action_cable.url = ENV.fetch("ACTION_CABLE_FRONTEND_URL") { "wss://localhost:28080" }
+    config.action_cable.url = ENV.fetch("ACTION_CABLE_FRONTEND_URL") { "wss://odin-twitter-production.up.railway.app:28080" }
 
         # Only allow connections to Action Cable from these domains.
-        origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS") { "http:\/\/localhost*" }.split(",")
+        origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS") { "https://odin-twitter-production.up.railway.app/" }.split(",")
         origins.map! { |url| /#{url}/ }
         config.action_cable.allowed_request_origins = origins
 
