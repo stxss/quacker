ba38dc996b3dd8cb9351a7a54188eca49a1cde90
stxss <93729036+stxss@users.noreply.github.com>
2023-11-20 13:48:21 +0000

    update search controller and where the form will be present

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
+    return @posts = [] if params[:q].blank? || params[:q].values.first.match?(/^\s*$/)
+
     @query = Tweet.ransack(params[:q])
     @posts = @query.result(distinct: true).includes(:author, :likes, :comments, :retweets, :quote_tweets).order(created_at: :desc)
   end
