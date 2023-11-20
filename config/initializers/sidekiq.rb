73f683762d53a15bf539c63de45968e5c31d18bf
stxss <93729036+stxss@users.noreply.github.com>
2023-11-20 12:21:23 +0000

    sidekiq config only in prod, and add specific env dev file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
-sidekiq_config = {url: ENV.fetch("REDIS_URL") { "redis://redis:6379/1" }}
+if Rails.env.production?
+  sidekiq_config = {url: ENV.fetch("REDIS_URL") { "redis://redis:6379/1" }}
 
-Sidekiq.configure_server do |config|
-  config.redis = sidekiq_config
-end
+  Sidekiq.configure_server do |config|
+    config.redis = sidekiq_config
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = sidekiq_config
+  Sidekiq.configure_client do |config|
+    config.redis = sidekiq_config
+  end
 end
