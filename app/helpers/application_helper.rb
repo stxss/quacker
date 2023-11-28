e9d4ebe507c2a2c38d94d0c9f67586f37de0c372
stxss <93729036+stxss@users.noreply.github.com>
2023-11-28 22:33:48 +0000

    Add svg's, update icon helpers and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,45 +1,7 @@
 module ApplicationHelper
-  def quote_icon_svg
-    content_tag(:svg, class: "icon-quote") do
-      content_tag(:path, "", d: "M14.23 2.854c.98-.977 2.56-.977 3.54 0l3.38 3.378c.97.977.97 2.559 0 3.536L9.91 21H3v-6.914L14.23 2.854zm2.12 1.414c-.19-.195-.51-.195-.7 0L5 14.914V19h4.09L19.73 8.354c.2-.196.2-.512 0-.708l-3.38-3.378zM14.75 19l-2 2H21v-2h-6.25z")
-    end
+  def backdrop
+    content_tag(:div, "", id: "backdrop", class: "hidden fixed top-0 left-0 z-10 w-screen h-full scale-[2]")
   end
-
-  def comment_icon_svg
-    content_tag(:svg, class: "icon-comment") do
-      content_tag(:path, "", d: "M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z")
-    end
-  end
-
-  def more_info_svg
-    content_tag(:svg, class: "icon-more-info") do
-      content_tag(:path, "", d: "M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z")
-    end
-  end
-
-  def share_icon_svg
-    content_tag(:svg, class: "icon-share") do
-      content_tag(:path, "", d: "M12 2.59l5.7 5.7-1.41 1.42L13 6.41V16h-2V6.41l-3.3 3.3-1.41-1.42L12 2.59zM21 15l-.02 3.51c0 1.38-1.12 2.49-2.5 2.49H5.5C4.11 21 3 19.88 3 18.5V15h2v3.5c0 .28.22.5.5.5h12.98c.28 0 .5-.22.5-.5L19 15h2z")
-    end
-  end
-
-  def copy_link_svg
-    content_tag(:svg, class: "icon-link h-6 w-6") do
-      content_tag(:path, "", d: "M18.36 5.64c-1.95-1.96-5.11-1.96-7.07 0L9.88 7.05 8.46 5.64l1.42-1.42c2.73-2.73 7.16-2.73 9.9 0 2.73 2.74 2.73 7.17 0 9.9l-1.42 1.42-1.41-1.42 1.41-1.41c1.96-1.96 1.96-5.12 0-7.07zm-2.12 3.53l-7.07 7.07-1.41-1.41 7.07-7.07 1.41 1.41zm-12.02.71l1.42-1.42 1.41 1.42-1.41 1.41c-1.96 1.96-1.96 5.12 0 7.07 1.95 1.96 5.11 1.96 7.07 0l1.41-1.41 1.42 1.41-1.42 1.42c-2.73 2.73-7.16 2.73-9.9 0-2.73-2.74-2.73-7.17 0-9.9z")
-    end
-  end
-
-  def send_via_message_svg
-    content_tag(:svg, class: "icon-send-via-message h-6 w-6") do
-      content_tag(:path, "", d: "M1.998 5.5c0-1.381 1.119-2.5 2.5-2.5h15c1.381 0 2.5 1.119 2.5 2.5v13c0 1.381-1.119 2.5-2.5 2.5h-15c-1.381 0-2.5-1.119-2.5-2.5v-13zm2.5-.5c-.276 0-.5.224-.5.5v2.764l8 3.638 8-3.636V5.5c0-.276-.224-.5-.5-.5h-15zm15.5 5.463l-8 3.636-8-3.638V18.5c0 .276.224.5.5.5h15c.276 0 .5-.224.5-.5v-8.037z")
-    end
-  end
-
-  # def svg
-  #   content_tag(:svg, class: "icon-") do
-  #     content_tag(:path, "", d: "")
-  #   end
-  # end
 
   # https://www.hotrails.dev/turbo-rails/flash-messages-hotwire
   def render_flash_message
--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -22,22 +22,20 @@ module IconsHelper
       repost_status = "stroke-reposted"
       submit_content = "Undo Repost"
     else
-      repost_status = "stroke-text"
+      repost_status = ""
       submit_content = "Repost"
     end
 
     repost_status += " opacity-50" if post.author.account.private_visibility && current_user != post.author
-    svg = inline_svg_tag("svg/repost.svg", class: "#{repost_status} z-20")
+    svg = inline_svg_tag("svg/repost.svg", class: repost_status)
     if submit
       text_submit = content_tag(:p, submit_content)
-      content = content_tag(:div, svg + text_submit, class: "flex gap-2")
-      type = "submit"
+      content = content_tag(:div, svg + text_submit, class: "flex gap-2 w-full whitespace-nowrap")
     else
       counter = content_tag(:div, post.reposts_count, id: dom_id(post, :reposts_count))
       content = content_tag(:div, svg + counter, class: "flex gap-2")
-      type = "button"
     end
-    (post.reposted_by?(current_user) || current_user == post.author || !post.author.account.private_visibility) ? button_tag(type: type) { content } : content
+    ((!post.reposted_by?(current_user) || current_user == post.author || !post.author.account.private_visibility) && submit) ? content_tag(:div, content, class: "flex") : button_tag(type: "button", class: "flex") { content }
   end
 
   def reposted_class(post)
@@ -46,11 +44,48 @@ module IconsHelper
 
   def quote_btn_icon(post)
     repost_status = post.reposted_by?(current_user) ? "stroke-reposted" : "stroke-text"
+    svg = inline_svg_tag("svg/quote.svg", class: "fill-none #{repost_status}")
     text_submit = content_tag(:p, "Quote")
+    content = content_tag(:div, svg + text_submit, class: "flex gap-2 w-full whitespace-nowrap")
+    content_tag(:div, content, class: "flex")
+  end
+
+  def comment_btn_icon(post)
+    comment_status = post.commented_by?(current_user) ? "stroke-comment" : "stroke-text"
+    svg = inline_svg_tag("svg/comment.svg", class: "fill-none #{comment_status}")
+    content_tag(:div, svg)
+  end
+
+  def commented_class(post)
+    "text-comment" if post.commented_by?(current_user)
+  end
+
+  def more_info_btn_icon
+    # flex gap-2 z-10 w-full whitespace-nowrap
+    button_tag(type: "button") { inline_svg_tag("svg/ellipsis.svg", class: "stroke-text") }
+  end
+
+  def share_btn_icon
+    button_tag(type: "button") { inline_svg_tag("svg/share.svg", class: "stroke-text") }
+  end
+
+  def copy_link_btn_icon
+    button_tag(type: "button", class: "flex gap-2 align-middle") { inline_svg_tag("svg/link.svg", class: "stroke-text") + "Copy Link to Post" }
+  end
+
+  def send_via_message_btn_icon
+    content_tag(:div, inline_svg_tag("svg/send_via_message.svg", class: "stroke-text") + "Send via Direct Message", class: "flex gap-2 align-middle")
   end
 
   def delete_btn_icon
     content_tag(:div, inline_svg_tag("svg/trash.svg", class: "text-red-600") + "Delete", class: "flex gap-2 align-middle")
   end
+
+  def pin_btn_icon
+    content_tag(:div, inline_svg_tag("svg/pin.svg", class: "stroke-text") + "Pin to your Profile", class: "flex gap-2 align-middle")
+  end
+
+  def change_who_can_reply_btn_icon
+    content_tag(:div, inline_svg_tag("svg/change_replies.svg", class: "stroke-text") + "Change who can reply", class: "flex gap-2 align-middle")
   end
 end
